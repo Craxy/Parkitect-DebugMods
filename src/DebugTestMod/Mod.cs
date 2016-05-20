ecdc5b7ffee093826afcc17fa236c53252735208
Craxy <Craxyss@gmail.com>
2016-05-20 20:17:37 +0200

    added: test/example mod

---
+++ src/DebugTestMod/Mod.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+
+namespace DebugTestMod
+{
+    public sealed class Mod : IMod
+    {
+        public string Name { get { return "Debug Test Mod"; } }
+        public string Description { get { return "Test/example mod to demonstrate debugging a Parkitect mod."; } }
+        public string Identifier { get; set; }
+
+        private GameObject _go = null;
+
+        public void onEnabled()
+        {
+            Debug.Log(string.Format("{0} enabled", Name));
+
+            _go = new GameObject();
+            _go.AddComponent<Logic>();
+        }
+
+        public void onDisabled()
+        {
+            Debug.Log(string.Format("{0} disabled", Name));
+
+            UnityEngine.Object.Destroy(_go);
+            _go = null;
+        }
+    }
+
+    public sealed class Logic : MonoBehaviour
+    {
+        private void Update()
+        {
+            if (Input.GetKey(KeyCode.LeftControl) & Input.GetKeyDown(KeyCode.F9))
+            {
+                Debug.Log("Ctrl+F9");
+            }
+            else if (Input.GetKey(KeyCode.LeftControl) & Input.GetKeyDown(KeyCode.F10))
+            {
+                Debug.Log("Ctrl+F10 -> throwing exception");
+                throw new Exception("Ctrl+F10");
+            }
+        }
+    }
+}
